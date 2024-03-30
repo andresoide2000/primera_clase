3cb435f9e09c09b36657a6aa7c1dde5a472e37c9
Andres <A01781321@tec.mx>
2024-03-29 20:47:01 -0600

    version final

--- Web/tarea_2/tarea_2.js
+++ Web/tarea_2/tarea_2.js
@@ -1,5 +1,5 @@
 import express from "express"; // import express
-let cards = []
+let cards = [];
 
 const app = express(); // create an express app
 app.use(express.json()); // use the express.json middleware
